68b7271647464e9687037556e30892ccbc943676
sakuraba_yusuke <>
2024-01-05 13:27:42 +0900

    #12 海底地形名を出力するための SQL 出力機能を実装

--- src/main.py
+++ src/main.py
@@ -17,6 +17,19 @@ class Application(Enum):
         return [i.value for i in cls]
 
 
+class SubmarineTopographyApp(Enum):
+    OUT_ALL_POINT_OF_SUBMARINE_TOPOGRAPHY_CSV = "out_all_point_of_submarine_topography_as_csv"
+    OUTPUT_SUBMARINE_TOPOGRAPHY_POINT_INSERT_SQL = "output_submarine_topography_point_insert_sql"
+
+    @classmethod
+    def get_names(cls) -> list:
+        return [i.name for i in cls]
+
+    @classmethod
+    def get_values(cls) -> list:
+        return [i.value for i in cls]
+
+
 class CoralApp(Enum):
     OUT_ALL_CORALS_CSV = "out_all_coral_as_csv"
     OUTPUT_CORAL_INSERT_SQL = "output_coral_insert_sql"
@@ -64,11 +77,16 @@ def display(stdscr, menu: list[str]) -> str:
 def menu(stdscr):
     selected_app = display(stdscr, Application.get_values())
     if selected_app == Application.SUBMARINE_TOPOGRAPHY.value:
-        SubmarineTopographyApplication().out_all_point_of_submarine_topography_as_csv()
+        selected_method = display(stdscr, SubmarineTopographyApp.get_values())
+        submarine_app = SubmarineTopographyApplication()
+        if selected_method == SubmarineTopographyApp.OUT_ALL_POINT_OF_SUBMARINE_TOPOGRAPHY_CSV.value:
+            submarine_app.out_all_point_of_submarine_topography_as_csv()
+        if selected_method == SubmarineTopographyApp.OUTPUT_SUBMARINE_TOPOGRAPHY_POINT_INSERT_SQL.value:
+            submarine_app.output_submarine_topography_point_insert_sql()
     elif selected_app == Application.CORAL.value:
         selected_method = display(stdscr, CoralApp.get_values())
         coral_app = CoralApplication()
-        if selected_method == CoralApp.OUT_ALL_CORALAS_CSV.value:
+        if selected_method == CoralApp.OUT_ALL_CORALS_CSV.value:
             coral_app.out_all_coral_as_csv()
         if selected_method == CoralApp.OUTPUT_CORAL_INSERT_SQL.value:
             coral_app.output_coral_insert_sql()
---
+++ src/submarine_topography/domain/umishiru/feature_type.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+
+class FeatureType(Enum):
+    UNDEFINED = (0, "未定義")
+    OTHER = (1, "その他")
+    BANK = (2, "堆")
+    BASIN = (3, "海盆")
+    KNOLL = (4, "海丘")
+    SPUR = (5, "海脚")
+    SUBMARINE_CANYON = (6, "海底谷")
+    SEAMOUNT = (7, "海山")
+    OBSTACLE = (8, "瀬・根・礁")
+    ESCARPMENT = (9, "海底崖")
+    TROUGH = (10, "舟状海盆")
+    GUYOT = (11, "平頂海山")
+    SUBMARINE_GRABEN = (12, "海底地溝")
+    HILL = (13, "海陵")
+    HOLE = (14, "海穴")
+    SADDLE = (15, "鞍部")
+    CALDERA = (16, "カルデラ")
+    SHELF_CHANNELS = (17, "陸棚谷群")
+    HILLS = (18, "海陵群")
+    KNOLLS = (19, "海丘群")
+    MATA = (20, "周辺凹地")
+    FAN = (21, "海底扇状地")
+    SEAMOUNTS = (22, "海山群")
+    SUBMARINE_CANYONS = (23, "海底谷群")
+    RISE = (24, "海膨")
+    BANKS = (25, "堆群")
+    HILL_CHAIN = (26, "小丘列")
+    RIDGE = (27, "海嶺")
+    TRENCH = (28, "海溝")
+    GAP = (29, "海裂")
+    PLATEAU = (30, "海台")
+    MUD_VOLCANO = (31, "泥火山")
+    CALDRON = (32, "海釜")
+    KNOLL_CHAIN = (33, "海丘列")
+    FAN_SHINKAI = (34, "深海扇状地")
+    SEA_MOUNT_CHAIN = (35, "海山列")
+    DEPRESSION = (36, "凹地")
+
+    @classmethod
+    def get_numeric_value(cls, name: str) -> int:
+        for enum in cls:
+            if enum.value[1] == name:
+                return enum.value[0]
+        return cls.UNDEFINED.value[0]
